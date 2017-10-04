e33d8fee98c36a4854b5c32dbe70a676d8ffd567
Jonathan Kirst <jskirst@gmail.com>
2017-10-04 22:29:32 +0000

    Widths between words and punctuation was all off

--- src/components/Part.vue
+++ src/components/Part.vue
@@ -2,7 +2,7 @@
   <div class="part">
     <div v-if="part.type == 'text'" class="part-text">{{ part.content }}</div>
     <v-select v-model="part.input" v-if="part.type == 'select'" :items="part.options" placeholder="...."></v-select>
-    <v-text-field v-model="part.input" v-if="part.type == 'short_text'" placeholder="......" dark single-line></v-text-field>
+    <v-text-field v-model="part.input" v-if="part.type == 'short_text'" placeholder="......" type="number" :style="{ width: part.characters + 'ch'}" dark required></v-text-field>
   </div>
 </template>
 
@@ -25,15 +25,24 @@ export default {
 
   .part-text {
     padding: 10px 0 0 0;
+    white-space: pre-wrap;
   }
 
  .input-group--text-field {
-    padding: 13px 0 0 10px;
+    padding: 13px 0 0 0;
+  }
+
+  .part .input-group__details {
+    min-height: 0px;
   }
 
   .input-group--text-field input {
     font-size: 20px;
     width: 6em;
+  }
+
+  .input-group--select .input-group__selections__comma {
+    font-size: 20px;
   }
 </style>
 
