42f8881221c311389ef5e987e2c5e7b3053c0626
Jonathan Kirst <jskirst@gmail.com>
2017-10-05 21:12:14 +0000

    Fully switched to plugin

---
+++ build/webpack.browser.conf.js
@@ -0,0 +1,39 @@
+var path = require('path')
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+var env = config.build.env
+
+var webpackConfig = merge(baseWebpackConfig, {
+  output: {
+    filename: 'edwards-voice.js',
+    libraryTarget: 'window',
+    library: 'EdwardsVoice'
+  },
+  externals: {
+    'vue': 'Vue',
+    'vuetify': 'Vuetify',
+    'axios': 'axios'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': env
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: false
+    }),
+    // extract css into its own file
+    new ExtractTextPlugin({
+      filename: utils.assetsPath('css/edwards-voice.css')
+    }),
+  ]
+})
+
+module.exports = webpackConfig
